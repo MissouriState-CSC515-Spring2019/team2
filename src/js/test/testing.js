2385309b4ef051d75164bf4b8aa7dd03bf7f3350
Nathan <weathersbee2@live.missouristate.edu>
2019-05-01 00:55:53 -0500

    test framework

---
+++ src/js/test/testing.js
@@ -0,0 +1,10 @@
+//Tests using Unit.js with Mocha
+
+var test = require('unit.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RandomFox from './RandomFox';
+import RandomCat from './RandomCat';
+import RandomDog from './RandomDog';
+
+test.object(RandomFox)
