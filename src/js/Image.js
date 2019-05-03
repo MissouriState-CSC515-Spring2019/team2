136116d1d845b8ed1112d02d7fca8b433f9360e1
HoviJ <jkh12@live.missouristate.edu>
2019-05-03 15:39:09 -0500

    mp4 and webm files will now load on randomDog

--- src/js/Image.js
+++ src/js/Image.js
@@ -9,17 +9,36 @@ class Image  extends React.Component {
                 </div>
             )
         else if(window.location.pathname.split('/')[window.location.pathname.split('/').length-1] === "randomDog")
+            if (window.location.hash.substr(1).split('.').pop() === 'mp4') {
+                return (
+                <div>
+                    <video controls>
+                    <source className="img-fluid" src={"https://random.dog/" + window.location.hash.substr(1)} type="video/mp4"/>
+                    </video>
+                </div>
+                )
+    
+            } else if (window.location.hash.substr(1).split('.').pop() === 'webm') {
+                return (
+                <div>
+                    <video controls>
+                    <source className="img-fluid" src={"https://random.dog/" + window.location.hash.substr(1)} type="video/webm"/>
+                    </video>
+                </div>
+                )
+                }    
+                else 
+                    return (
+                        <div className="container text-center">
+                            <img className="img-fluid" src={"https://random.dog/" + window.location.hash.substr(1)}/>
+                        </div>
+                    )
+        else if(window.location.pathname.split('/')[window.location.pathname.split('/').length-1] === "randomFox")
             return (
                 <div className="container text-center">
-                    <img className="img-fluid" src={"https://random.dog/" + window.location.hash.substr(1)}/>
+                    <img className="img-fluid" src={"http://randomfox.ca/images/" + window.location.hash.substr(1)}/>
                 </div>
             )
-        else if(window.location.pathname.split('/')[window.location.pathname.split('/').length-1] === "randomFox")
-        return (
-            <div className="container text-center">
-                <img className="img-fluid" src={"http://randomfox.ca/images/" + window.location.hash.substr(1)}/>
-            </div>
-        )
     }
         
 }
