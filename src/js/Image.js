1e7aa8f480c51849181e6a068626d755c1c7be5e
HoviJ <jkh12@live.missouristate.edu>
2019-05-02 20:02:15 -0500

    added styling changes to center images

--- src/js/Image.js
+++ src/js/Image.js
@@ -4,19 +4,19 @@ class Image  extends React.Component {
     render() {
         if(window.location.pathname.split('/')[window.location.pathname.split('/').length-2] === "randomCat")
             return (
-                <div>
+                <div className="container text-center">
                     <img className="img-fluid" src={"https://purr.objects-us-east-1.dream.io/i/" + this.props.match.params.cat}/>
                 </div>
             )
         else if(window.location.pathname.split('/')[window.location.pathname.split('/').length-2] === "randomDog")
             return (
-                <div>
+                <div className="container text-center">
                     <img className="img-fluid" src={"https://random.dog/" + this.props.match.params.dog}/>
                 </div>
             )
         else if(window.location.pathname.split('/')[window.location.pathname.split('/').length-2] === "randomFox")
         return (
-            <div>
+            <div className="container text-center">
                 <img className="img-fluid" src={"http://randomfox.ca/images/" + this.props.match.params.fox}/>
             </div>
         )
