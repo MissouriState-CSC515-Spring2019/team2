ea45f84885beba8eb89a0e4a3584bf9effbaf896
Dustin Scott <dustin001@live.missouristate.edu>
2019-05-04 13:46:14 -0500

    fox social media sharing working

--- src/js/Home.js
+++ src/js/Home.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
     constructor(props) {
         super(props);
         this.fetchData = this.fetchData.bind(this);
-        this.state = {current_URL: ''}; //this holds the current link for facebook sharing (NOT the localhost url)
+        this.state = {URL_for_social_media: ''}; //this holds the current link for social-media sharing (NOT the localhost url)
       }
 
     fetchData(api) {
@@ -21,17 +21,20 @@ class Home extends React.Component {
                 if(api === "https://aws.random.cat/meow") {
                     this.props.history.push("/randomCat#" + (result.file.split('/')[result.file.split('/').length-1]));
                     this.setState({
-                        current_URL: 'https://purr.objects-us-east-1.dream.io/i/' + window.location.hash.substr(1)
+                        URL_for_social_media: 'https://purr.objects-us-east-1.dream.io/i/' + window.location.hash.substr(1)
                     });
                 }
                 else if(api === "https://random.dog/woof.json") {
                     this.props.history.push("/randomDog#" + (result.url.split('/')[result.url.split('/').length-1]));
                     this.setState({
-                        current_URL: 'https://random.dog/' + window.location.hash.substr(1)
+                        URL_for_social_media: 'https://random.dog/' + window.location.hash.substr(1)
                     });
                 }
                 else if(api === "https://cors-anywhere.herokuapp.com/https://randomfox.ca/floof/") {
-                    this.props.history.push("/randomFox#" + (result.image.split('/')[result.image.split('/').length-1]))
+                    this.props.history.push("/randomFox#" + (result.image.split('/')[result.image.split('/').length-1]));
+                    this.setState({
+                        URL_for_social_media: 'http://randomfox.ca/images/' + window.location.hash.substr(1)
+                    });
                 }
             }
         )
@@ -60,9 +63,9 @@ class Home extends React.Component {
 			</div>
             <div className="row">
 				<h6 className="col">
-                    <Facebook url={this.state.current_URL} />
-                    <Twitter url={this.state.current_URL} />
-                    <Pinterest url={this.state.current_URL} />
+                    <Facebook url={this.state.URL_for_social_media} />
+                    <Twitter url={this.state.URL_for_social_media} />
+                    <Pinterest url={this.state.URL_for_social_media} />
 				</h6>
 			</div>
         </div>
